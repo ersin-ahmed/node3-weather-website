598fe1b44e149ace745c021b9996b481c554184d
ersin-ahmed <ersinahmeddev@gmail.com>
2022-01-10 00:07:54 +0200

    Added wind speed info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const forecast = (lat, long, callback) => {
         // feelslikeTemp: response.body.current.feelslike,
         // humidity: response.body.current.humidity,
         // weatherDesc: response.body.current.weather_descriptions[0],
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees. The humidity is ${body.current.humidity}%.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees. The Humidity is ${body.current.humidity}%. The Wind Speed is ${body.current.wind_speed} kmh.`
       );
     }
   });
